1f80ae822e58c8b9d67ab1edadaac41aa62d774c
NE7K <sjjang16@naver.com>
2025-05-13 13:53:11 +0900

    Docs : create analaysis csv file

--- UpStock-DeepLearning.py
+++ UpStock-DeepLearning.py
@@ -3,6 +3,16 @@ import tensorflow as tf
 
 from langchain_community.document_loaders import WebBaseLoader
 
+import yfinance as yf
+
+# Part Get Yahoo Finance Data
+StockData = yf.download('^NDX', start='2000-01-01', end='2025-05-13')
+
+print(StockData)
+
+exit()
+
+# Part Save CSV file
 
 # Part Get Data Collection for news title
 loader = WebBaseLoader('https://www.cnbc.com/world/?region=world')
@@ -14,3 +24,9 @@ title_data2 = loader2.load()
 
 print(title_data)
 print(title_data2)
+
+# Part Insert Data CSV (Supabase)
+
+# Part Load Data CSV file
+
+# Part 
