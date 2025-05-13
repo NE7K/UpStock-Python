5e81ba6efce14fee6ec653f2294991202bcb7340
NE7K <sjjang16@naver.com>
2025-05-13 12:55:13 +0900

    Feat : Get news title data

--- UpStock-DeepLearning.py
+++ UpStock-DeepLearning.py
@@ -1,3 +1,16 @@
 import pandas as pd
 import tensorflow as tf
 
+from langchain_community.document_loaders import WebBaseLoader
+
+
+# Part Get Data Collection for news title
+loader = WebBaseLoader('https://www.cnbc.com/world/?region=world')
+title_data = loader.load()
+
+loader2 = WebBaseLoader('https://finviz.com/news.ashx')
+title_data2 = loader2.load()
+
+
+print(title_data)
+print(title_data2)
