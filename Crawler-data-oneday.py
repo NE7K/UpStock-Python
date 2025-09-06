b2f52e6296f7277b9f3545c116e455d3805fe09b
NE7K <sjjang16@naver.com>
2025-09-07 00:36:31 +0900

    Feat : one day parse stock price

---
+++ Crawler-data-oneday.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from langchain_community.document_loaders import WebBaseLoader
+import yfinance as yf
+import numpy as np
+
+# note regex import
+import re
+
+# Get Yahoo Finance Data
+StockData = yf.download('^NDX', start='2025-09-05', end='2025-09-06')
+
+print(StockData)
