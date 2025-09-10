7eaa80b88444c3349864e3763b076357058086d8
NE7K <sjjang16@naver.com>
2025-09-10 10:57:47 +0900

    Docs : crawler-data file and deeplearning upstock file backup

--- Crawler-data.py
+++ Crawler-data.py
@@ -1,66 +1,37 @@
 import pandas as pd
+# from langchain_community.document_loaders import WebBaseLoader
 import yfinance as yf
 import numpy as np
 
-# langchain import
-# from langchain_community.document_loaders import WebBaseLoader
+# note regex import
+import re
 
 # Get Yahoo Finance Data
-StockData = yf.download('^NDX', start='2009-02-14', end='2020-06-12')
-
-# create independence dataset 
-StockData = StockData.copy()
+try:
+    StockData = yf.download(
+        '^NDX', # stock number
+        start='2009-02-14',
+        end='2020-06-12',
+        auto_adjust=True, # 과거 주가와 현재 주가의 차이점을 완화 병합 혹은 분할 그리고 상승으로 인한 차이
+        progress=True
+    )
+    
+except Exception as e:
+    print(f'Download Fail : {e}')
+    
 # 인덱스 초기화
 StockData.reset_index(inplace=True)
 
-# TODO Date -> date인데 이거 제거하고 다 바꾸기
-StockData.rename(columns={'Date' : 'date'}, inplace=True)
-
-# 멀티 플렉스 > 단일, 다른 개별 주식 이용하면 확장필요
+# 멀티 플렉스 > 단일
 StockData.columns = StockData.columns.droplevel(1)
 
 # open 가격 때보다 close 가격이 높으면 1 else 0
-# INFO 오늘 장중에 올랐는지 안올랐는지를 표기하는건데 필요없음 주가 계산해보면 의미없는 결과물
-# StockData['label'] = np.where( StockData['Open'] < StockData['Close'], 1, 0)
+StockData['label'] = np.where( StockData['Open'] < StockData['Close'], 1, 0)
 
-# PART 로그 수익률, 로그 비율, 로그 거래량 칼럼 추가
-# prev close 전날 종가
-StockData['prev_Close'] = StockData['Close'].shift(1)
-# 전일 대비 증가 로그수익률
-StockData['Close_logret'] = np.log(StockData['Close'] / StockData['prev_Close'])
-# 저가/종가 로그비율 : 하방변동성
-StockData['Low_logret'] = np.log(StockData['Low'] / StockData['Close'])
-# 고가/종가 로그비율 : 상방변동성
-StockData['High_logret'] = np.log(StockData['High'] / StockData['Close'])
-# 시가/종가 로그비율
-StockData['Open_logret'] = np.log(StockData['Open'] / StockData['Close'])
-# 로그 1+거래량
-StockData['Volume_log'] = np.log1p(StockData['Volume'])
+# analyst_ratings_processed date 명시로인한 통일성 부여
+StockData.rename(columns={'Date' : 'date'}, inplace=True)
+StockData.to_csv('DataSets/stock_price_data.csv', index=False)
 
-# BUG log label 내일 종가가 오늘 종가보다 오를지 안오를지 살펴볼 수 있음
-# StockData['label_logret'] = np.where(StockData['Close_logret'] > 0, 1, 0)
-
-# 데이터 누수 막기
-StockData['future_Close'] = StockData['Close'].shift(-1)
-StockData['label_next_up'] = (StockData['future_Close'] > StockData['Close']).astype(int)
-
-# 첫 raw의 prev_Close 제거, NaN 정리 (첫행 prev_Close, 마지막행 future_Close)
-StockData = StockData.dropna(subset=['prev_Close', 'future_Close']).reset_index(drop=True)
-
-# 5) 저장 (권장 컬럼 순서로)
-column = [
-    'date', 'Open', 'High', 'Low', 'Close', 'Volume',
-    'prev_Close',
-    'Close_logret', 'High_logret', 'Low_logret', 'Open_logret', 'Volume_log',
-    'future_Close',
-    'label_next_up'           
-]
-
-StockData[column].to_csv('DataSets/stock_price_data_v2.csv', index=False)
-# shape 
-print('csv shape', StockData.shape)
-
-# TODO 현재 진행하지 않는 부분
 # RSI, MACD
 # StockData['Rsi'] = 
 
