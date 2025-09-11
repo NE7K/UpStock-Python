ffd4a7a4973dd7d1705ea111a53e04e261b44f5b
NE7K <sjjang16@naver.com>
2025-09-12 00:34:41 +0900

    Feat : Added supabase connect

--- upstock-sentiment.py
+++ upstock-sentiment.py
@@ -8,10 +8,14 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 import pickle
-import supabase
 import os
 import time
 import datetime
+
+import supabase
+import hashlib # hash
+from supabase import create_client, Client
+from dotenv import load_dotenv # env with os
 
 # validation x > split Test Data
 from sklearn.model_selection import train_test_split
@@ -21,6 +25,46 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.callbacks import TensorBoard, EarlyStopping
 
 from finvizfinance.news import News
+
+# env load
+load_dotenv()
+
+supabase_url = os.getenv('SupaBase_Url')
+supabase_key = os.getenv('SupaBase_Key')
+
+# connect sb sdk
+supabase: Client = create_client(supabase_url, supabase_key)
+
+# supabase storage
+def download_model_file():
+    bucket_name = 'sentiment_file'
+    
+    file_paths = [
+        'upstock_sentiment_model.keras',
+        'upstock_sentiment_model.h5',
+        'upstock_sentiment_tokenizer.pickle'
+    ]
+
+    os.makedirs('SaveModel', exist_ok=True) # exist no error
+    bucket = supabase.storage.from_(bucket_name)
+    
+    for file_path in file_paths:
+        try:
+            res = bucket.download(file_path)
+
+            # download() 결과가 bytes 또는 Response일 수 있음
+            content = res.read() if hasattr(res, "read") else res
+            local_path = os.path.join("SaveModel", os.path.basename(file_path))
+            
+            with open(local_path, "wb") as f:
+                f.write(content)
+
+            print(f"{file_path} download complete {local_path}")
+
+        except Exception as e:
+            print(f"{file_path} download fail : {e}")
+
+download_model_file()
 
 # 경로
 sentiment_path = 'DataSets/upstock-sentiment-data.csv' # sentiment data
@@ -56,7 +100,7 @@ def check_all_model(path, description):
         print(f'{description} not exists')
         return None
 
-# INFO load tokenizer => TextVectorization로 변경 가능성 유의
+# load tokenizer => TextVectorization로 변경 가능성 유의
 def load_pickle(path, description):
     if os.path.exists(path):
         try:
@@ -117,7 +161,7 @@ tokenizer = load_pickle(tokenizer_path, 'Tokenizer')
 # save model exists => predict
 # save model not exists => DeepLearning
 if os.path.exists(model_path) and os.path.exists(tokenizer_path):
-
+    
     # def load_news():
     #     fnews = News()
     #     # dictionary
@@ -125,21 +169,24 @@ if os.path.exists(model_path) and os.path.exists(tokenizer_path):
     #     news_df = all_news['news']
     #     predict_texts = news_df['Title'].tolist() # predict texts
     #     # print(news_df['Title'][1])
-        
-    fnews = News()
-    all_news = fnews.get_news()
-    news_df = all_news['news'] 
-    # TODO load and to csv
-
-    # 문자열을 datetime으로 변환 (예: 'Sep-11-25 09:30AM')
-    news_df['parsed_date'] = pd.to_datetime(news_df['Date'], errors='coerce')
-
-    today = datetime.date.today()
-    today_news = news_df[news_df['parsed_date'].dt.date == today]
-
-    predict_texts = today_news['Title'].tolist()
     
-    # TEST predict data set
+    # parse except task
+    try:
+        fnews = News()
+        all_news = fnews.get_news()
+        news_df = all_news['news'] 
+    except Exception as e:
+        print(f'finviz news parse fail : {e}')
+
+    # string to date time
+    news_df['parsed_date'] = pd.to_datetime(news_df['Date'], errors='coerce') # format 지정
+
+    today = datetime.date.today() # today
+    today_news = news_df[news_df['parsed_date'].dt.date == today] # today == parse data date
+
+    predict_texts = today_news['Title'].tolist() # insert pare data
+    
+    # past predict data
     # predict_texts = [
     #     "EM portfolios funnel near $45 billion in August but cracks are showing, IIF says", # Negative
     #     "Stocks' Bull Market Nears 3-Year Anniversary. It Likely Has More Room to Run.",    # Positive
@@ -154,25 +201,55 @@ if os.path.exists(model_path) and os.path.exists(tokenizer_path):
     prediction = model.predict(predict_data)
     # print(prediction)
     
+    # past division predict data task
     # for text, prob in zip(predict_texts, prediction):
     #     label = 'positive' if prob[0] >= 0.7 else 'negative'
     #     print(f'[{label}] {text}\n : {prob[0]:.2f}\n') # :.2f
+    
+    #  TODO
+    # 1. 중립적인 혹은, 예측이 애매한 기사 거르기 = x
+    # 2. to csv 파일로 예측값과 같이 supabase에 저장
+    # https://supabase.com/docs/reference/python/upsert
+    
+    sb_result = []  # sending to supabase predict data
         
-    for text, prob in zip(predict_texts, prediction):
-        score = prob[0]
-        if score >= 0.6:
+    for text, percent in zip(predict_texts, prediction):
+        # 강한 긍정과 강한 부정만 끌어다가 쓰기
+        score = float(percent[0])
+        if score >= 0.8:
             label = "positive"
-        elif score <= 0.4:
+        elif score <= 0.3:
             label = "negative"
         else:
-            label = "neutral"
-        print(f"[{label}] {text}\n : {score:.2f}\n")
+            continue
         
-    #  TODO
-    # 1. 중립적인 혹은, 예측이 애매한 기사 거르기
-    # 2. to csv 파일로 예측값과 같이 supabase에 저장
-    # 3. 
+        print(f"[{label}] {text}\n : {score:.2f}\n") # :.2f
+    
+        sb_result.append({
+            'text': text,
+            'percent': score, # BUG type error float32
+            'label': label,
+            'source': 'finviz',
+            'run_at': datetime.datetime.now(datetime.timezone.utc).isoformat(), # utc time
+            'hash': hashlib.sha256(text.encode("utf-8")).hexdigest() # hash 256bit -> 64자리, 16진수
+        })
 
+    if sb_result:
+        try:
+            response = (
+                supabase.table('news_sentiment')
+                .upsert(sb_result, on_conflict='hash')
+                .execute()
+            )
+            print(f'supbase upload complete : {len(response.data)}')
+            
+        except Exception as e:
+            print(f'supabase upload fail : {e}')
+            
+    else:
+        print('upload data not exist')
+    
+    
 else:
     print('Sentiment Model and Tokenizer is not exists, Start DeepLearning')
     
