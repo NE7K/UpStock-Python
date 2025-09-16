f44d46e5802e3f49b6637fa91f2d10ecf25f9086
NE7K <sjjang16@naver.com>
2025-09-17 01:35:05 +0900

    Feat : Added footnote

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# INFO compatibility issue import
+# from keras.preprocessing.text import Tokenizer
+# from keras.callbacks import EarlyStopping
+# from keras.preprocessing.sequence import pad_sequences
+# from keras.callbacks import TensorBoard
+
 # pip github connect | pip freeze > piplist.txt
 from upstock.storage.model_downloader import download_model_file
 from upstock.builders.pipeline import run_pipeline
